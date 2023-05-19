1dcb3ef09c8ed87912b9c87bb4d17ece967d11c9
Cynthia <caron.cynthia@outlook.fr>
2023-05-19 13:27:21 +0200

    improve documentation

--- README.md
+++ README.md
@@ -5,8 +5,12 @@
 - Node.js v16.19.1 or latest
 - react v18.2.0 or latest
 - react-dom v18.2.0 or latest
+- [IntelliJ IDEA](https://www.jetbrains.com/idea/) or [VScode](https://code.visualstudio.com/) are recommended
 
 ## Installation
+
+[Link to package](https://www.npmjs.com/package/@cynthiaaa_crn/success-modal/v/0.1.2?activeTab=readme)
+
 To install, you can use [npm](https://npmjs.org/) or [yarn](https://yarnpkg.com):
 ```
 $ npm install --save @cynthiaaa_crn/success-modal
--- src/lib/SuccessModal.js
+++ src/lib/SuccessModal.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./SuccessModal.css";
 
+/**
+ * SuccessModal is a reusable component that represents a modal displaying a success message or content.
+ * It is used to provide visual feedback to the user when a successful action or operation has occurred.
+ *
+ * @component
+ * @param {boolean} open - Controls the visibility of the success modal. If true, the modal is displayed; otherwise, it is hidden.
+ * @param {function} setOpen - Callback function to control the open state of the success modal.
+ * @param {Object} containerStyle - Custom CSS styles to be applied to the success modal container.
+ * @param {ReactNode} children - The content to be displayed within the success modal.
+ * @returns {JSX.Element|null} - The rendered SuccessModal component.
+ */
 function SuccessModal({ open, setOpen, containerStyle = {}, children }) {
   if (open) {
     return (
